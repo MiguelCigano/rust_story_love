bc15377d35256028cc4f42a96e958e3213dd2951
MiguelCigano <miguel_m0130@hotmail.com>
2024-12-24 23:31:53 -0800

    add code rust

--- RustToEveryone/basic/dependencias.rs
+++ RustToEveryone/basic/dependencias.rs
@@ -1,5 +1,30 @@
+/* this faile is about the variable mutable 
+* the name is a litle diferent
+*/
+
+const NUMBER_SPACES:i32 = 12;
+
 fn main()
 {
-    let num
-    
+    // first case: using variable mutable
+    let mut num = 10;
+    println!("Number value is: {}", num);
+    num =  2;
+    println!("Number value is: {}", num);
+    let _num = 123; // this prefix is used about annoucenments warning compilers
+
+    //  second case: use let to change value
+    // this concept is know like SHADOWING
+    let x = 20;
+    // put more here TODO
+    let x =  x + 5;
+    println!("value x is: {}", x);
+
+    // third case: 
+    let space = "   ";
+    println!("Input is: {}", space);
+    let space = space.len();
+    println!("number of spaces is: {}", space);
+
+    println!("default number of space: {}", NUMBER_SPACES);
 }
