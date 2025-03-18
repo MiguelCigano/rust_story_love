7ffb7533a2a718210475ce40a3dfd93b4a2983eb
MiguelCigano <miguel_m0130@hotmail.com>
2025-03-17 23:13:28 -0700

    add some examples using rust

---
+++ RustToEveryone/basic/functions_00/src/main.rs
@@ -0,0 +1,10 @@
+/// La palabra clave fn permite que tu declares una nueva función
+
+fn main() {
+    println!("Hola mundo!");
+    another_function();
+}
+
+fn another_function() {
+    println!("Hola desde otra funcion!");
+}
---
+++ RustToEveryone/struct01/target/debug/deps/enum01-adc11fc98a0aab53.d
@@ -0,0 +1,5 @@
+/home/jmiguel-nitro/Documents/rust/rust_long_story_love/RustToEveryone/struct01/target/debug/deps/enum01-adc11fc98a0aab53: src/main.rs
+
+/home/jmiguel-nitro/Documents/rust/rust_long_story_love/RustToEveryone/struct01/target/debug/deps/enum01-adc11fc98a0aab53.d: src/main.rs
+
+src/main.rs:
